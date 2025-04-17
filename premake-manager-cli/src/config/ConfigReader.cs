4a6b03f14417276f050d0a27d09f81dbdc5dcca8
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-04-18 00:46:37 +0200

    added: - ConfigWriter - ConfigReader

---
+++ premake-manager-cli/src/config/ConfigReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
+using System.IO;
+
+namespace src.config
+{
+    internal class ConfigReader
+    {
+        public string version { get; set; }
+        public IList<PremakeModule> modules { get; set; }
+        public ConfigReader(string path = "")
+        {
+            string configPath;
+
+            if (string.IsNullOrWhiteSpace(path))
+                configPath = Path.Combine(Directory.GetCurrentDirectory(), "premakeConfig.yml");
+            else
+                configPath = Path.Combine(path, "premakeConfig.yml");
+            
+
+            string deserializedConfig = File.Exists(configPath) ? File.ReadAllText(configPath) : "";
+
+            //If the desirialized config is not null or whitespace we want to read it.
+            if (!string.IsNullOrWhiteSpace(deserializedConfig))
+            {
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)  // see height_in_inches in sample yml 
+                    .Build();
+               ConfigReader tempInstance = deserializer.Deserialize<ConfigReader>(deserializedConfig);
+
+               modules = tempInstance.modules;
+               version = tempInstance.version;
+            }
+        }
+    }
+}
---
+++ premake-manager-cli/src/config/ConfigWriter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
+using System.IO;
+
+namespace src.config
+{
+    /// <summary>
+    /// This class is a builder style class but writes to the premakeConfig.yml instead of returning a class instance
+    /// </summary>
+    internal class ConfigWriter
+    {
+        private string version {  get; set; }
+        private IList<PremakeModule> modules { get; set; }
+        ConfigWriter()
+        {
+        }
+        /// <summary>
+        /// Sets the version of the config
+        /// </summary>
+        /// <param name="versionTag"></param>
+        /// <returns></returns>
+        public ConfigWriter SetVersion(string versionTag)
+        {
+            this.version = versionTag;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a module to the Configuration
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
+        public ConfigWriter AddModule(PremakeModule module)
+        {
+            modules.Add(module);
+            return this;
+        }
+
+        /// <summary>
+        /// Removes a Module from the configuration
+        /// </summary>
+        /// <param name="moduleName"></param>
+        /// <returns></returns>
+        public ConfigWriter RemoveModule(string moduleName)
+        {
+            PremakeModule foundModule = modules.First(module => module.name.Equals(moduleName));
+            modules.Remove(foundModule);
+            return this;
+        }
+
+        public async Task Write(string path = "") 
+        {
+            var serializer = new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+
+            string serializedConfig = serializer.Serialize(this);
+
+            string outputPath;
+
+            if (string.IsNullOrWhiteSpace(path))
+                outputPath = Path.Combine(Directory.GetCurrentDirectory(), "premakeConfig.yml");    
+            else
+                outputPath = Path.Combine(path, "premakeConfig.yml");
+            
+            await File.WriteAllTextAsync(outputPath, serializedConfig);
+        }
+    }
+}
